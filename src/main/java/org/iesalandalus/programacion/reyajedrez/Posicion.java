3a6e2d6582a474211a6b26606baaffe08726a71c
aida_ <afabaco831@ieslosangeles.es>
2023-11-10 16:49:18 +0100

    Creacion de equals y haschode

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 
     //Variables
@@ -48,4 +50,19 @@ public class Posicion {
         }
         this.columna = columna;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
+
+
 }
