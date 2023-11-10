7046edd9fdbedb7cd36777a0bcdab08d8d3765ca
aida_ <afabaco831@ieslosangeles.es>
2023-11-10 14:01:36 +0100

    Creacion de getters y setters

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -2,6 +2,50 @@ package org.iesalandalus.programacion.reyajedrez;
 
 public class Posicion {
 
+    //Variables
+
     private int fila;
     private char columna;
+
+    // Constructor parámetros
+    public Posicion (int fila, char columna){
+
+        setFila(fila);
+        setColumna(columna);
+    }
+
+    //Constructor copia
+
+    public Posicion (Posicion posicion){
+
+        setFila(posicion.getFila());
+        setColumna(posicion.getColumna());
+
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+
+        if (fila <1 || fila>8) {
+
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+
+        if (columna <'a' || columna>'h') {
+
+            throw new IllegalArgumentException("ERROR: Columa no válida.");
+        }
+        this.columna = columna;
+    }
 }
