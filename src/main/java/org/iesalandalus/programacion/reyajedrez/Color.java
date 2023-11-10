79221466b174f6cf2eb982c4e8098246bb8d34e0
aida_ <afabaco831@ieslosangeles.es>
2023-11-10 12:56:46 +0100

    Creación de Enum Color y Direccion con constructores

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Color.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.reyajedrez;
+
+public enum Color { BLANCO("Blanco"), NEGRO("Negro");
+
+    private String cadenaAMostrar;
+
+    private Color(String cadenaAMostrar){
+
+        this.cadenaAMostrar=cadenaAMostrar;
+
+    }
+
+
+}
--- src/main/java/org/iesalandalus/programacion/reyajedrez/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Direccion.java
@@ -1,5 +1,20 @@
 package org.iesalandalus.programacion.reyajedrez;
 
-public enum Direccion { NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE,
-    NOROESTE, ENROQUE_CORTO, ENROQUE_LARGO
+public enum Direccion { NORTE("Norte"), NORESTE("Noreste"),
+    ESTE("Este"), SURESTE("Sureste"), SUR("Sur"), SUROESTE("Suroeste"), OESTE("Oeste"),
+    NOROESTE("Noroeste"), ENROQUE_CORTO("Enroque corto"), ENROQUE_LARGO("Enroque largo");
+
+    private String cadenaAMostrar;
+
+    private Direccion(String cadenaAMostrar){
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
 }
+
+
+
