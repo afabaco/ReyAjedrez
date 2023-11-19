9f73c9c3a0101f91c6d9069adb4ca7d00343c360
aida_ <afabaco831@ieslosangeles.es>
2023-11-19 13:59:59 +0100

    creacion clase mover y tostring

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
 
     private Color color;
@@ -63,7 +65,99 @@ public class Rey {
         }
     }
 
+    // Método mover
+
+    public void mover (Direccion direccion) throws OperationNotSupportedException {
+
+        int nuevaFila=posicion.getFila();
+        char nuevaColumna= posicion.getColumna();
+        totalMovimientos=0;
+
+        if (direccion == null) {
+
+            throw new NullPointerException("ERROR: se sale del tablero.");
+        }
+
+        switch (direccion) {
+
+            case NORTE:
+
+                nuevaFila++;
+                break;
+
+            case NORESTE:
+
+                nuevaFila++;
+                nuevaColumna++;
+                break;
+
+            case ESTE:
+                nuevaColumna++;
+                break;
+
+            case SURESTE:
+                nuevaFila--;
+                nuevaColumna++;
+                break;
+
+            case SUR:
+                nuevaFila--;
+
+            case SUROESTE:
+                nuevaFila--;
+                nuevaColumna--;
+                break;
+
+            case OESTE:
+                nuevaColumna--;
+                break;
+
+            case NOROESTE:
+                nuevaFila++;
+                nuevaColumna--;
+                break;
+
+            case ENROQUE_CORTO:
+                if (totalMovimientos>0) {
+
+                throw new NullPointerException("ERROR: movimiento no válido. ");
+
+                } else {
+
+                    nuevaColumna++;
+                    nuevaColumna++;
+
+                } break;
+
+            case ENROQUE_LARGO:
+                if (totalMovimientos>0) {
+
+                    throw new NullPointerException("ERROR: movimiento no válido.");
+                } else {
+
+                    nuevaColumna--;
+                    nuevaColumna--;
+                }
+
+            default: setPosicion(new Posicion (nuevaFila, nuevaColumna));
 
 
 
+
+
+
+
+
+
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Rey{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                '}';
+    }
 }
